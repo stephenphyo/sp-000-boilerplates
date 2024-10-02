7f25fc60a75856f2d039241d7766e4fd7e6bbdb2
Stephen Phyo <stephenphyo2019.official@gmail.com>
2024-10-02 08:35:55 +0000

    v01: Initial Setup

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+/*** Provider Imports ***/
+import { HelmetProvider } from 'react-helmet-async';
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
+    </StrictMode>,
+)
