58a360839c7e00ae704bbabf21f53257605c055e
Stephen Phyo <stephenphyo2019.official@gmail.com>
2024-10-07 01:55:49 +0000

    v02: Added SASS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 /*** CSS Imports ***/
-import './App.css';
+import './App.scss';
 
 /*** Package Imports ***/
 import { Toaster } from 'react-hot-toast';
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      'styles': '/src/styles',
       'pages': '/src/pages'
     },
   },
